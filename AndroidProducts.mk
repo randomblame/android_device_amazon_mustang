b0f79e061be77bd18712fc2e7f558826ea182cdc
kelsi <dumbandroid@gmail.com>
2020-01-17 21:53:08 -0800

    Mustang: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,13 @@
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_mustang.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mustang-userdebug
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo lineage_mustang-userdebug
-add_lunch_combo lineage_mustang-eng
